74ebec8cac7c00b10a77b5d289335fc767148205
Stousn <stefan.reip@edu.fh-joanneum.at>
2019-04-22 19:53:45 +0200

    webapp: themeing added

--- ionic_electron/xPlatformIosDemoApp/src/app/tab3/tab3.page.ts
+++ ionic_electron/xPlatformIosDemoApp/src/app/tab3/tab3.page.ts
@@ -5,4 +5,45 @@ import { Component } from '@angular/core';
   templateUrl: 'tab3.page.html',
   styleUrls: ['tab3.page.scss']
 })
-export class Tab3Page {}
+export class Tab3Page {
+
+  darkmode: boolean
+
+  private DARK: string = '(prefers-color-scheme: dark)'
+
+  private LIGHT: string = '(prefers-color-scheme: light)'
+
+  constructor() {
+    this.init()
+  }
+
+  private init() {
+
+    this.detectColorScheme()
+  }
+
+  private detectColorScheme() {
+    if (!window.matchMedia) {
+      this.darkmode = false
+      return
+    }    
+    const mqDark = window.matchMedia(this.DARK)
+    if (mqDark.matches) {
+      this.darkmode = true
+    } else {
+      this.darkmode = false
+    }
+  }
+
+  colorShemeChanged() {
+    console.log("Darkmode:" + this.darkmode)
+
+    if (this.darkmode) {
+      document.documentElement.style.setProperty('--ion-background-color', '#333')
+      document.documentElement.style.setProperty('--ion-text-color', '#CCC')
+    } else {
+      document.documentElement.style.setProperty('--ion-background-color', '')
+      document.documentElement.style.setProperty('--ion-text-color', '')
+    }
+  }
+}
